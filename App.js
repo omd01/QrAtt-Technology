4f3547ead68b3e914ca4bed686d19433b1e096ce
Om Dahale <odtech.in@gmail.com>
2023-02-05 01:28:48 -0800

    Update

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import Main from './Main'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 const App = () => {
   return (
+    <Provider store={store}>
    <Main/>
+   </Provider>
   )
 }
 
